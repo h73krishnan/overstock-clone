0b73e05f0e848dd0968fabd64f5cd0d2608821ae
amaryamunavishwakarma <vishwakarmaamar805@mail.com>
2022-05-05 23:09:11 +0530

    amar

--- product Page/product_page.js
+++ product Page/product_page.js
@@ -226,3 +226,26 @@ function myFunction1() {
       }
     }
   }
+
+
+
+
+
+  
+  function myFunctionaccount() {
+    document.getElementById("myDropdownacc").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementById("myDropdownacc");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
